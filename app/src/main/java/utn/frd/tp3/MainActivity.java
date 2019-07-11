50603b275e9ef4f98c67d11225413696d20202db
brios <brios@itport.com.ar>
2019-07-11 19:50:22 -0300

    ejemplo GET Sergio

--- app/src/main/java/utn/frd/tp3/MainActivity.java
+++ app/src/main/java/utn/frd/tp3/MainActivity.java
@@ -2,7 +2,16 @@ package utn.frd.tp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Handler del botón
+        findViewById(R.id.btnEjecutar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MiAsyncTask().execute();
+            }
+
+        });
+
     }
+
+    private class MiAsyncTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            return RESTService.makeGetRequest(
+                    "https://jsonplaceholder.typicode.com/posts/1");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast notificacion = Toast.makeText(
+                    getApplicationContext(), result, Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+
+    private static class RESTService{
+        public static String makeGetRequest(String restURL){
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL( restURL );
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+
+                BufferedReader bReader = new BufferedReader(
+                        new InputStreamReader(inputStream, "utf-8"), 8);
+                StringBuilder sBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = bReader.readLine()) != null) {
+                    sBuilder.append(line + "\n");
+                }
+                inputStream.close();
+
+                result = sBuilder.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return result;
+
+        }
+
+    }
+
 }
